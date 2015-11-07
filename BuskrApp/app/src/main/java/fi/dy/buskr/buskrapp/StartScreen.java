6d50e16467c287004d12fa0eb96fd938daa3c5bc
Janne Kemppainen <kemppainen.janne@gmail.com>
2015-11-07 13:24:42 +0200

    BuskrApp as a starting point

--- BuskrApp/app/src/main/java/fi/dy/buskr/buskrapp/StartScreen.java
+++ BuskrApp/app/src/main/java/fi/dy/buskr/buskrapp/StartScreen.java
@@ -1,4 +1,4 @@
-package com.example.janne.buskr;
+package fi.dy.buskr.buskrapp;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,16 +8,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.junctionhacks.buskr.MESSAGE";
+public class StartScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -34,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_start_screen, menu);
         return true;
     }
 
@@ -52,12 +49,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-/*    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }*/
 }
